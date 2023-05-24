4ad3a1ffe9691af31f3dcd2015714daebbca82d9
Francisco Borja Lobato <fjbl2788@gmail.com>
2023-05-24 18:21:37 -0500

    Solve warnings

--- src/components/trySwap.jsx
+++ src/components/trySwap.jsx
@@ -1,55 +1,72 @@
-import { getQuote } from '../redux/quotes.js';
-import Web3 from 'web3';
-import BigNumber from 'bignumber.js';
+//import { useEffect } from 'react';
+//import { getQuote } from '../redux/quotes.js';
+//import Web3 from 'web3';
+//import BigNumber from 'bignumber.js';
 
-const TrySwap = async (selectedToken) => {
-  const erc20abi = [
-    {
-      "inputs": [
-        { "internalType": "string", "name": "name", "type": "string" },
-        { "internalType": "string", "name": "symbol", "type": "string" },
-        { "internalType": "uint256", "name": "max_supply", "type": "uint256" }
-      ],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    // Rest of the ABI...
-  ];
+//const TrySwap = (selectedToken) => {
+//  useEffect(() => {
+//    const executeSwap = async () => {
+//      const erc20abi = [
+//        {
+//          "inputs": [
+//            { "internalType": "string", "name": "name", "type": "string" },
+//            { "internalType": "string", "name": "symbol", "type": "string" },
+//            { "internalType": "uint256", "name": "max_supply", "type": "uint256" }
+//          ],
+//          "stateMutability": "nonpayable",
+//          "type": "constructor"
+//        },
+//        // Rest of the ABI...
 
-  console.log("trying swap");
+//        {
+//          "inputs": [],
+//          "name": "name",
+//          "outputs": [{ "internalType": "string", "name": "", "type": "string" }],
+//          "stateMutability": "view",
+//          "type": "function"
+//        }
+//      ];
 
-  // Only work if MetaMask is connected
-  // Connecting to Ethereum: Metamask
-  const web3 = new Web3(window.ethereum);
+//      console.log("trying swap");
 
-  // The address, if any, of the most recently used account that the caller is permitted to access
-  const accounts = await window.ethereum.request({ method: "eth_accounts" });
-  const takerAddress = accounts[0];
-  console.log("takerAddress: ", takerAddress);
+//      // Only work if MetaMask is connected
+//      // Connecting to Ethereum: Metamask
+//      const web3 = new Web3(window.ethereum);
 
-  const swapQuoteJSON = await getQuote(takerAddress);
+//      // The address, if any, of the most recently used account that the caller is permitted to access
+//      const accounts = await window.ethereum.request({ method: "eth_accounts" });
+//      const takerAddress = accounts[0];
+//      console.log("takerAddress: ", takerAddress);
 
-  // Set Token Allowance
-  // Set up approval amount
-  const fromTokenAddress = selectedToken.from.address;
-  const maxApproval = new BigNumber(2).pow(256).minus(1);
-  console.log("approval amount: ", maxApproval);
-  const ERC20TokenContract = new web3.eth.Contract(erc20abi, fromTokenAddress);
-  console.log("setup ERC20TokenContract: ", ERC20TokenContract);
+//      const swapQuoteJSON = await getQuote(takerAddress);
 
-  // Grant the allowance target an allowance to spend our tokens.
-  const tx = await ERC20TokenContract.methods.approve(
-    swapQuoteJSON.allowanceTarget,
-    maxApproval.toString()
-  )
-    .send({ from: takerAddress })
-    .then(tx => {
-      console.log("tx: ", tx)
-    });
+//      // Set Token Allowance
+//      // Set up approval amount
+//      const fromTokenAddress = selectedToken.from.address;
+//      const maxApproval = new BigNumber(2).pow(256).minus(1);
+//      console.log("approval amount: ", maxApproval);
+//      const ERC20TokenContract = new web3.eth.Contract(erc20abi, fromTokenAddress);
+//      console.log("setup ERC20TokenContract: ", ERC20TokenContract);
 
-  // Perform the swap
-  const receipt = await web3.eth.sendTransaction(swapQuoteJSON);
-  console.log("receipt: ", receipt);
-}
+//      // Grant the allowance target an allowance to spend our tokens.
+//      const tx = await ERC20TokenContract.methods.approve(
+//        swapQuoteJSON.allowanceTarget,
+//        maxApproval.toString()
+//      )
+//        .send({ from: takerAddress })
+//        .then(tx => {
+//          console.log("tx: ", tx)
+//        });
 
-export default TrySwap;
+//      // Perform the swap
+//      const receipt = await web3.eth.sendTransaction(swapQuoteJSON);
+//      console.log("receipt: ", receipt);
+//    };
+
+//    executeSwap();
+//  }, [selectedToken]);
+
+//  return null; // Or you can return a JSX component if needed
+//};
+
+//export default TrySwap;
